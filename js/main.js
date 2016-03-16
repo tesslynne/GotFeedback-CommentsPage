969e66cd17a82481081958b59eefe7df1eba9cd0
Tess DiNapoli <Tess DiNapoli>
2016-03-15 18:44:02 -0700

    Update styling, add EventListener, add TimeStamp

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-function submitFeedback() {
+
+document.getElementById("theBtn").addEventListener("click", submitForm)
+
+function submitForm() {
 
 	var user = document.getElementById("userName").value //initial user input
 	var feedback = document.getElementById("feedbackHere").value //user feedback in form
-	
-	var userName = document.createTextNode(user + " says...")  
+	var timeStamp = new Date().toDateString()
+	var userName = document.createTextNode("on " + timeStamp + "  " + user + " says...")
 	var comment = document.createTextNode(feedback)
 
 	var userContent = document.createElement("h4")
 	userContent.appendChild(userName)
+
 	var commentBody = document.createElement("p")
 	commentBody.appendChild(comment)
+
+
+	var divWrapper = document.createElement("div") // for wrapping h4 and p in one div
+
 	var listUsers = document.createElement("li")
 	
 	listUsers.className = "userlist"
-	commentBody.className= "well well-lg"
+	divWrapper.className= "well well-lg"
 
 
-	listUsers.appendChild(userContent)
-	listUsers.appendChild(commentBody)
+	divWrapper.appendChild(userContent) //appending h4 and p to div to serve as wrapper
+	divWrapper.appendChild(commentBody) 
+	listUsers.appendChild(divWrapper) //append to li
 	document.getElementById("listAll").appendChild(listUsers)	
+
 
 
 }
